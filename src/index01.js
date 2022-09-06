609523b18410ce71df29d9f225d52686cdbaf914
zimu <zimu@ywwl.com>
2022-09-06 18:13:07 +0800

    feat: init webpack

---
+++ src/index01.js
@@ -0,0 +1,3 @@
+var age = 18;
+
+const name = "Tom";
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack'); // 访问内置的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    pageOne: './src/index.js',
+    pageTwo: './src/index01.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ]
+}
